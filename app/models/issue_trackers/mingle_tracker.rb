c5069d6bea4cbfe80af089fec20236c85af4b129
Arunram Kalaiselvan <arun@edengroups.com>
2012-11-08 17:22:29 -0500

    Updating mingle tracker to use http instead of https

--- app/models/issue_trackers/mingle_tracker.rb
+++ app/models/issue_trackers/mingle_tracker.rb
@@ -31,12 +31,12 @@ class IssueTrackers::MingleTracker < IssueTracker
 
   def create_issue(problem, reported_by = nil)
     properties = ticket_properties_hash
-    basic_auth = account.gsub(/https?:\/\//, "https://#{username}:#{password}@")
+    basic_auth = account.gsub(/https?:\/\//, "http://#{username}:#{password}@")
     Mingle.set_site "#{basic_auth}/api/v1/projects/#{project_id}/"
 
     card = Mingle::Card.new
     card.card_type_name = properties.delete("card_type")
-    card.name = issue_title(problem)
+    card.name = '[Errbit] ' + issue_title(problem)
     card.description = body_template.result(binding)
     properties.each do |property, value|
       card.send("cp_#{property}=", value)
